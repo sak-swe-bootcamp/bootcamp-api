20655dea9389bda78eeab591ac0e1582bf35b69d
sakofsuken <thgin.sir@gmail.com>
2025-01-27 19:23:31 +0900

    validatorが誤っていたのを修正

--- src/blogs/post.ts
+++ src/blogs/post.ts
@@ -1,6 +1,6 @@
+import { zValidator } from '@hono/zod-validator';
 import type { Blog } from '@prisma/client/edge';
 import { Hono } from 'hono';
-import { validator } from 'hono/validator';
 import { z } from 'zod';
 import { db } from '../lib/db.js';
 
@@ -8,22 +8,27 @@ const schema = z.object({
   title: z.string(),
   content: z.string(),
   userName: z.string(),
-  userImage: z.string(),
+  userImage: z.string().optional(),
 });
 
+const DEFAULT_USER_IMAGE_URL = 'https://avatars.githubusercontent.com/u/42';
+
 export const postBlog = new Hono().post(
-  validator('form', (value, c) => {
-    const parsed = schema.safeParse(value);
-    if (!parsed.success) {
+  zValidator('json', schema, (result, c) => {
+    if (!result.success) {
       return c.text('paramsが正しくありません', 400);
     }
-    return parsed.data;
   }),
   async (c) => {
     try {
-      const { title, content, userName, userImage } = c.req.valid('form');
+      const { title, content, userName, userImage } = c.req.valid('json');
       const blog: Blog = await db.blog.create({
-        data: { title, content, userName, userImage },
+        data: {
+          title,
+          content,
+          userName,
+          userImage: userImage || DEFAULT_USER_IMAGE_URL,
+        },
       });
       return c.json(blog, 201);
     } catch (error) {
